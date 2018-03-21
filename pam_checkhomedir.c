d6bf459c2d37a819153d085ecd3f44c9fbb336be
Mark Coccimiglio <mc76@rice.edu>
2018-03-21 15:53:55 -0500

    Working BETA V0.0.1

--- pam_checkhomedir.c
+++ pam_checkhomedir.c
@@ -19,64 +19,117 @@
 
 */
 
-#define PAM_SM_AUTH
-
 #ifndef __PCHD_VERSION__
-  #define __PCHD_VERSION__  "0.0.0"
-  #define __PCHD_VERSION_D__ 0.0.0
+  #define __PCHD_VERSION__  "0.0.1"
+  #define __PCHD_VERSION_D__ 0.0.1
+  #define __COPYRIGHT__ 2018
   #define __AUTHOR__ "Mark Coccimiglio"
   #define __AUTHOR_EMAIL__ "mcoccimiglio@rice.edu"
   #define __APP__ pam_checkhomedir
 #endif
 
+#define PAM_SM_AUTH
+#define PAM_SM_SESSION
+
 #include <security/pam_modules.h>
 #include <security/_pam_macros.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/mount.h>
 #include <pwd.h>
 #include <dirent.h>
-#include <string.h>
 #include <syslog.h>
 
+#define __DEBUG__ 1
+
+int checkhomedir (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
+      const char  *service;
+      const char  *user;
+      const char  *authtok;
+
+      if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:checkhomedir");
+      openlog(NULL , LOG_PID, LOG_AUTHPRIV);
+
+      if ( pam_get_item(pamh, PAM_SERVICE, (const void **)(const void *)&service ) != PAM_SUCCESS || !service || !*service) {
+          syslog (LOG_NOTICE, "Unable to retrieve the PAM service name for :%s STOP.", service);
+          closelog();
+          return (PAM_AUTH_ERR);
+        }
+      if (__DEBUG__) syslog(LOG_NOTICE, "DEBUG:We have service '%s' ...continue. ", service);
+      if (pam_get_item( pamh, PAM_USER, (const void **)(const void *)&user ) != PAM_SUCCESS || !user || !*user) {
+          /* User name is not set.  Tell pam to get user name */
+          if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:No user provided,  Asking PAM for username.");
+          if ( pam_get_user (pamh, &user, NULL) != PAM_SUCCESS || !user || !*user ) {
+           syslog (LOG_NOTICE, "pam_checkhomedir(%s:auth): Unable to retrieve the PAM user name, is NULL, or zero length, for '%s' ", service, user);
+           closelog();
+           return (PAM_USER_UNKNOWN);
+         }
+      }
+
+      /* Validate User */
+      struct passwd *_userInfo=getpwnam(user);
+      if (! _userInfo) {
+        char __tempNotice[256]={0};
+        syslog (LOG_NOTICE, "pam_checkhomedir(%s:auth): Unable to locate user ID : '%s' STOP.", service, user);
+        closelog();
+        return (PAM_USER_UNKNOWN);
+      }
+      if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:We have validated user: '%s' ...continue.", user);
+
+      DIR *_homeDIR;
+      if ( (_homeDIR=opendir(_userInfo->pw_dir) ) ) {
+        if (__DEBUG__) syslog (LOG_NOTICE, "pam_checkhomedir(%s:auth): Validated home directory: '%s' ", service, _userInfo->pw_dir);
+        closedir (_homeDIR);
+        closelog();
+        return (PAM_SUCCESS);
+      }
+      closedir (_homeDIR);
+      if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:we have NOT validated home dir: '%s' ...continue.", _userInfo->pw_dir);
+
+      return (PAM_PERM_DENIED);
+}
+
 PAM_EXTERN int
- pam_sm_authenticate
-  (pam_handle_t *pamh, int flags, int argc, const char *argv[])
-   {
-     return (PAM_IGNORE);
-   }
+  pam_sm_authenticate
+   (pam_handle_t *pamh, int flags, int argc, const char **argv) {
+     if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:pam_checkhomedir:pam_sm_authenticate");
+     return ( checkhomedir(pamh, flags, argc, argv) );
+
+}
 
 PAM_EXTERN int
   pam_sm_setcred
-   (pam_handle_t *pamh,int flags,int argc, const char **argv)
-    {
-      return (PAM_IGNORE);
-    }
+   (pam_handle_t *pamh, int flags, int argc, const char **argv) {
+     if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:pam_checkhomedir:pam_sm_setcred");
+     return ( checkhomedir(pamh, flags, argc, argv) );
+}
+
 PAM_EXTERN int
-    pam_sm_acct_mgmt
-    (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
-    	return (PAM_IGNORE);
+  pam_sm_acct_mgmt
+    (pam_handle_t *pamh, int flags, int argc, const char **argv) {
+      if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:pam_checkhomedir:pam_sm_acct_mgmt");
+      return ( checkhomedir(pamh, flags, argc, argv) );
 }
 
 PAM_EXTERN int
   pam_sm_open_session
-     (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
-    	  return (PAM_IGNORE);
+    (pam_handle_t *pamh, int flags, int argc, const char **argv) {
+      if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:pam_checkhomedir:pam_sm_open_session");
+      return ( checkhomedir(pamh, flags, argc, argv) );
 }
 
 PAM_EXTERN int
   pam_sm_close_session
-    (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
-    	return (PAM_IGNORE);
+    (pam_handle_t *pamh, int flags, int argc, const char **argv) {
+      if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:pam_checkhomedir:pam_sm_close_session");
+    	return ( PAM_IGNORE );
 }
 
 PAM_EXTERN int
   pam_sm_chauthtok
-    (pam_handle_t *pamh, int flags, int argc, const char *argv[]) {
-      return (PAM_IGNORE);
+    (pam_handle_t *pamh, int flags, int argc, const char **argv) {
+      if (__DEBUG__) syslog (LOG_NOTICE, "DEBUG:pam_checkhomedir:pam_sm_chauthtok");
+      return ( PAM_IGNORE );
 }
 
 #ifdef PAM_MODULE_ENTRY
